fb00c382a52895acc7b8f27ab66d317934ed880c
Benjahhz <78978553+Benjahhz@users.noreply.github.com>
2022-12-08 20:59:11 -0300

    Add files via upload

--- assets/js/modules/home/scrollAnimation.js
+++ assets/js/modules/home/scrollAnimation.js
@@ -11,8 +11,8 @@ const observer = new IntersectionObserver((entries) => {
         entry.target.classList.add("--active");
         $(entry.target).find('.seccion-nosotros__item__title').addClass('animate__animated animate__fadeInLeft animate__faster');
         $(entry.target).find('.seccion-nosotros__item__text').addClass('animate__animated animate__fadeInRight animate__faster');
-        $(entry.target).find('.desarrollo__item__image').eq(0).addClass('animate__animated animate__fadeInLeft animate__normal');
-        $(entry.target).find('.desarrollo__item__image').eq(1).addClass('animate__animated animate__fadeInRight animate__normal');
+        $(entry.target).find('.desarrollo__item').eq(0).find('.desarrollo__item__image').addClass('animate__animated animate__fadeInLeft animate__normal');
+        $(entry.target).find('.desarrollo__item').eq(1).find('.desarrollo__item__image').addClass('animate__animated animate__fadeInRight animate__normal');
         setTimeout(() => {
           $(entry.target).find('.desarrollo__item__image').eq(0).removeClass('animate__animated animate__fadeInLeft animate__normal');
           $(entry.target).find('.desarrollo__item__image').eq(1).removeClass('animate__animated animate__fadeInRight animate__normal');    
